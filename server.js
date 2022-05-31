fb0a409d58c8410357d8383b847370e2de17e318
harpal singh <harpal@dev-story.com>
2022-05-31 13:08:27 +0530

    first commit

--- server.js
+++ server.js
@@ -1,25 +1,36 @@
-const express = require("express");
-const cors = require("cors");
+const express =require("express");
+// const bodyParser =require("body-parser");
+ const cors =require("cors");
+ const db = require("./app/models");
+
 const app = express();
-var corsOptions = {
-  origin: "http://localhost:8081"
-};
-app.use(cors(corsOptions));
-// parse requests of content-type - application/json
-app.use(express.json());
-// parse requests of content-type - application/x-www-form-urlencoded
-app.use(express.urlencoded({ extended: true }));
-const db = require("./app/models");
-const tutorialModel = require("./app/models/tutorial.model");
-db.sequelize.sync();
-// simple route
-app.get("/", (req, res) => {
-  res.json({ message: "Welcome to bezkoder application." });
+
+ var corsOptions = {
+     origin:"http://localhost:8081"
+ }
+
+
+ //app.use();
+ app.use(cors(corsOptions));
+ app.use(express.json());
+ app.use(express.urlencoded({ extended: true }));
+ db.sequelize.sync();
+
+app.get("/", (req,res) => {
+    console.log("req.body",req.query)
+    res.json({message : "welcome to  dev story."});
 });
+
+// db.sequelize.sync({ force: true }).then(() =>{
+//     console.log("Drop and re-sync db.");
+// });
 const routes=require("./app/routes/tutorial.routes")(app);
 
-// set port, listen for requests
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
-});
+app.listen (PORT, () => {
+    console.log(`server is runing on port ${PORT}.`);
+})
+
+// parse requests of content-type - application/json
+
+// parse requests of content-type - application/x-www-form-urlencoded
